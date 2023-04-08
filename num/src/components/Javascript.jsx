770078def26fc05fd7f22f7668fc15d63d91e4d5
Reezywise <reezybiyi@gmail.com>
2023-04-08 14:38:37 +0100

    update

---
+++ num/src/components/Javascript.jsx
@@ -0,0 +1,24 @@
+'String';length
+'String';slice()
+'String';substring()
+'String';substr()
+'String';replace()
+'String'; replaceAll()
+'String'; toUpperCase()
+'String'; toLowerCase()
+'String'; concat()
+'String'; trim()
+'String'; trimStart()
+'String'; trimEnd()
+'String'; padStart()
+'String'; padEnd()
+'String'; charAt()
+'String'; charCodeAt()
+'String';split()
+
+
+//for loops
+let text = "reezy";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
